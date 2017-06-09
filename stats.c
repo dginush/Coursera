bdae6971e1b2425a9e55a84ce2193dade8070fb6
Dudi Tedgy <dtedgy@gmail.com>
2017-06-09 16:02:13 +0300

    jhghj

--- stats.c
+++ stats.c
@@ -9,16 +9,16 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief A program to manipulate and search through a given array
  *
- * <Add Extended Description Here>
+ * This program prints the statistics of a given array (Min, Max, Median & Mean),
+ * Sorts its elements (from largest to smallest) and prints the array.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Dudi Tedgy
+ * @date June 9, 2017
  *
  */
-
 
 
 #include <stdio.h>
@@ -29,15 +29,52 @@
 
 void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+    unsigned char test[SIZE] = {34, 201, 190, 154, 8, 194, 2, 6,
+                                114, 88, 45, 76, 123, 87, 25, 23,
+                                200, 122, 150, 90, 92, 87, 177, 244,
+                                201, 6, 12, 60, 8, 2, 5, 67,
+                                7, 87, 250, 230, 99, 3, 100, 90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+    /* prints the unsorted array */
+    printf("Unsorted array:\n");
+    print_array(test, SIZE);
+
+    /* sorts the array */
+    sort_array(test, SIZE);
+
+    /* prints the sorted array */
+    printf("Sorted array:\n");
+    print_array(test, SIZE);
+
+    /* prints the statistics of the array - Min, Max, Median & Mean */
+    print_statistics(test, SIZE);
+}
+
+void sort_array(unsigned char *test, unsigned int size) {
 
 }
 
-/* Add other Implementation File Code Here */
+void print_array(unsigned char *test, unsigned int size) {
+
+}
+
+void print_statistics(unsigned char *test, unsigned int size) {
+
+}
+
+unsigned char find_minimum(unsigned char *t, unsigned int size) {
+
+}
+
+unsigned char find_maximum(unsigned char *t) {
+
+}
+
+unsigned char find_mean(unsigned char *t, unsigned int size) {
+
+}
+
+unsigned char find_median(unsigned char *t, unsigned int size) {
+    
+}
+
--- stats.h
+++ stats.h
@@ -9,32 +9,111 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief A program to manipulate and search through a given array
  *
- * <Add Extended Description Here>
+ * This program prints the statistics of a given array (Min, Max, Median & Mean),
+ * Sorts its elements (from largest to smallest) and prints the array.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Dudi Tedgy
+ * @date June 9, 2017
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/**
+ * @brief Prints the statistics for an array: Min, Max, Median, Mean
+ *
+ * This function takes an unsigned char pointer to an n-elements data array and
+ * an unsigned integer as the size of the array and prints its statistics
+ *
+ * @param An unsigned char pointer to an n-element data array
+ * @param An unsigned integer as the size of the array
+ *
+ * @return None
+ */
+void print_statistics(unsigned char *, unsigned int);
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Prints the elements for an array
  *
- * <Add Extended Description Here>
+ * This function takes an unsigned char pointer to an n-elements data array and
+ * an unsigned integer as the size of the array and prints its elements
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param An unsigned char pointer to an n-element data array
+ * @param An unsigned integer as the size of the array
  *
- * @return <Add Return Informaiton here>
+ * @return None
  */
+void print_array(unsigned char *, unsigned int);
+
+/**
+ * @brief Prints the Median of an array
+ *
+ * This function takes an unsigned char pointer to an n-elements data array and
+ * an unsigned integer as the size of the array and returns its median
+ *
+ * @param An unsigned char pointer to an n-element data array
+ * @param An unsigned integer as the size of the array
+ *
+ * @return An unsigned char as the Median of the array
+ */
+unsigned char find_median(unsigned char *, unsigned int);
+
+/**
+ * @brief Prints the Mean of an array
+ *
+ * This function takes an unsigned char pointer to an n-elements data array and
+ * an unsigned integer as the size of the array and returns its Mean
+ *
+ * @param An unsigned char pointer to an n-element data array
+ * @param An unsigned integer as the size of the array
+ *
+ * @return An unsigned char as the Mean of the array
+ */
+unsigned char find_mean(unsigned char *, unsigned int);
+
+/**
+ * @brief Prints the Maximum of an array
+ *
+ * This function takes an unsigned char pointer to an n-elements data array
+ * and returns its Maximum
+ *
+ * @param An unsigned char pointer to an n-element data array
+ *
+ * @return An unsigned char as the Maximum of the array
+ */
+unsigned char find_maximum(unsigned char *);
+
+/**
+ * @brief Prints the Minimum of an array
+ *
+ * This function takes an unsigned char pointer to an n-elements data array and
+ * an unsigned integer as the size of the array and returns its Minimum
+ *
+ * @param An unsigned char pointer to an n-element data array
+ * @param An unsigned integer as the size of the array
+ *
+ * @return An unsigned char as the Minimum of the array
+ */
+unsigned char find_minimum(unsigned char *, unsigned int);
+
+/**
+ * @brief Sorts the array from the largest number to the smallest one
+ *
+ * This function takes an unsigned char pointer to an n-elements data array and
+ * an unsigned integer as the size of the array and sorts its elements from largest to smallest
+ *
+ * @param An unsigned char pointer to an n-element data array
+ * @param An unsigned integer as the size of the array
+ *
+ * @return None
+ */
+void sort_array(unsigned char *, unsigned int);
+
+
+
 
 
 #endif /* __STATS_H__ */
