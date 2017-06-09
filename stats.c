adb55d7dc35ac5b52c8e26603ba4586b27f546bd
David <dginush@gmail.com>
2017-06-09 16:14:15 +0300

    from git conflict

--- stats.c
+++ stats.c
@@ -50,7 +50,7 @@ void main() {
     print_statistics(test, SIZE);
 }
 
-void sort_array(unsigned char *test, unsigned int size) {
+void sort_array(unsigned char *test, unsigned int size) { //from git
     unsigned char tmp;
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size - (i + 1); ++j) {
